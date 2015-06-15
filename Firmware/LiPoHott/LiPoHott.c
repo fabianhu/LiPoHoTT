b91ab8390e55a7111d8c869ea9d6d4bc44cce9b4
fabianhu <fabianwho@gmx.net>
2015-06-16 00:52:57 +0200

    add compiler requirement as comment
    With the new compiler the text mode does not work.

--- Firmware/LiPoHott/LiPoHott.c
+++ Firmware/LiPoHott/LiPoHott.c
@@ -17,7 +17,8 @@
 	You should have received a copy of the GNU General Public License
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
- 
+ ATTENTION:
+ COMPILE WITH WINAVR 20100110
  
  TODO:
  - Kalibrierung checken
